7a61d2119b632a657a92b719ab50dadfb9f8e0fb
Thomas L'Anglais <tlang072@uottawa.ca>
2018-11-26 18:25:52 -0500

    moving from laptop to desktop

--- src/containers/AuthenticatedContainer/index.js
+++ src/containers/AuthenticatedContainer/index.js
@@ -1,23 +1,41 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { compose } from 'redux'
 import { connect } from "react-redux";
+import { firestoreConnect } from 'react-redux-firebase'
+
 import { signOut } from "../../store/actions/auth";
 
 class NavBar extends Component {
+  
   render() {
     return <nav>
       <Link to="/">Home</Link>
       <a href="/" onClick={this.props.signOut}>Log Out</a>
     </nav>;
   }
+
+  componentDidMount() {
+    console.log(this);
+  }
 }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    signOut: () => dispatch(signOut())
-  };
-};
-const ConnectedNavBar = connect(null, mapDispatchToProps)(NavBar);
+// const mapStateToProps = state => ({
+//   user: state.firestore.ordered.users
+// })
+
+// const mapDispatchToProps = dispatch => ({
+//   signOut: () => dispatch(signOut())
+// })
+
+// const ConnectedNavBar = compose(
+//   firestoreConnect(['users']),
+//   connect(mapStateToProps, mapDispatchToProps)
+// )(NavBar);
+
+const ConnectedNavBar = compose(
+  firestoreConnect((props) => )
+)(NavBar);
 
 export default class AuthenticatedContainer extends Component {
   render() {
