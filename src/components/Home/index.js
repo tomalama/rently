609d9736e8c96220b1abb64d97345f6c3007d77d
Thomas L'Anglais <thomas1446@gmail.com>
2018-12-03 15:23:11 -0500

    removed debug stuff from home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,9 @@
 // React
 import React, { Component } from "react";
 
-import { Link } from 'react-router-dom'
-
 class Home extends Component {
   render() {
-    return <div>Hey
-      <Link to='/demo'>demo</Link>
-    </div>;
+    return <div>Hey</div>;
   }
 }
 
