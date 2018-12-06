5c829d01110d768695578095810c764b84b01220
tomalama <tommy.nguyen7@hotmail.com>
2018-12-05 23:33:45 -0500

    Bounciness added to owner view properties

--- src/components/OwnerViewProperties/index.js
+++ src/components/OwnerViewProperties/index.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { compose } from 'redux'
+import { compose } from "redux";
 import { connect } from "react-redux";
-import { firestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase'
+import { firestoreConnect, isLoaded, isEmpty } from "react-redux-firebase";
+import { Redirect } from "react-router-dom";
 
 import PropertyCard from "../PropertyCard";
 import { Property } from "../../models/property";
@@ -10,23 +11,24 @@ import "./style.scss";
 function getOwnerList(userId, properties) {
   return properties.reduce((sum, propertyData) => {
     if (propertyData.userId === userId) {
-
-      sum.push(new Property(
-        propertyData.id,
-        propertyData.propertyType,
-        propertyData.numBedrooms,
-        propertyData.numBathrooms,
-        propertyData.numOtherRooms,
-        propertyData.rent,
-        propertyData.province,
-        propertyData.city,
-        propertyData.streetNumber,
-        propertyData.streetName,
-        propertyData.postalCode,
-        propertyData.location,
-        propertyData.deleted ? propertyData.deleted : false,
-        propertyData.imageURLs
-      ))
+      sum.push(
+        new Property(
+          propertyData.id,
+          propertyData.propertyType,
+          propertyData.numBedrooms,
+          propertyData.numBathrooms,
+          propertyData.numOtherRooms,
+          propertyData.rent,
+          propertyData.province,
+          propertyData.city,
+          propertyData.streetNumber,
+          propertyData.streetName,
+          propertyData.postalCode,
+          propertyData.location,
+          propertyData.deleted ? propertyData.deleted : false,
+          propertyData.imageURLs
+        )
+      );
     }
 
     return sum;
@@ -34,42 +36,46 @@ function getOwnerList(userId, properties) {
 }
 
 class OwnerList extends React.Component {
-  
-  
   render() {
-    const { auth, properties } = this.props;
-    const loaded = isLoaded(properties);
+    const { auth, properties, profile } = this.props;
+    if (isLoaded(profile)) {
+      if (!auth.uid) return <Redirect to="/login" />;
 
-    if (loaded) var ownerList = getOwnerList(auth.uid, properties);
+      if (profile.type !== "owner") return <Redirect to="/" />;
 
-    return (
-      <div className="owner-list">
-        <h2 className="owner-list__title">My Properties</h2>
+      const loaded = isLoaded(properties);
 
-        <div className="owner-list__properties">
-          {!loaded && <span>Loading</span>}
-          {loaded &&
-            ownerList &&
-            ownerList.map((property, i) => {
-              return <PropertyCard key={i} showStatus property={property} />;
-            })}
-          {loaded && isEmpty(ownerList) && (
-            <span>It's quiet here... Add some properties!</span>
-          )}
+      if (loaded) var ownerList = getOwnerList(auth.uid, properties);
+
+      return (
+        <div className="owner-list">
+          <h2 className="owner-list__title">My Properties</h2>
+
+          <div className="owner-list__properties">
+            {!loaded && <span>Loading</span>}
+            {loaded &&
+              ownerList &&
+              ownerList.map((property, i) => {
+                return <PropertyCard key={i} showStatus property={property} />;
+              })}
+            {loaded && isEmpty(ownerList) && (
+              <span>It's quiet here... Add some properties!</span>
+            )}
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
+    return <div />;
   }
 }
 
 const enhance = compose(
-  firestoreConnect(['properties']),
-  connect(
-    state => ({
-      auth: state.firebase.auth,
-      properties: state.firestore.ordered.properties
-    })
-  )
-)
+  firestoreConnect(["properties"]),
+  connect(state => ({
+    auth: state.firebase.auth,
+    properties: state.firestore.ordered.properties,
+    profile: state.firebase.profile
+  }))
+);
 
 export default enhance(OwnerList);
